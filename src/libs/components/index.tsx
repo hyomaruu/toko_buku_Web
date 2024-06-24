81fd093b73e7add9bdc11fd2853a96788df60ac6
Abid Fakhri Maulana <abidfakhrimaulana@Abids-MacBook-Pro.local>
2024-06-24 13:14:24 +0700

    remove unused component v2

--- src/libs/components/index.tsx
+++ src/libs/components/index.tsx
@@ -1,8 +1,6 @@
-export * from "./header/header.container";
 export * from "./loading/loading.container";
 
 export * from "./alert/alert.component";
-export * from "./card/card.component";
 export * from "./sidebar/sidebar.component";
 export * from "./searchbar/searchbar.component";
 
